691e5f76162f9bc5b90daf601338c16b9e1d506a
= <=>
2016-07-21 10:13:15 +0100

    minor changes to the dictionary loading script to display progress and now dump all the doc names on to the screen

--- ms2ldaviz/load_dict.py
+++ ms2ldaviz/load_dict.py
@@ -16,31 +16,52 @@ if __name__ == '__main__':
 	# filename = '/Users/simon/git/lda/notebooks/beer3.dict'
 	with open(filename,'r') as f:
 		lda_dict = pickle.load(f)
-	experiment = Experiment.objects.get_or_create(name='beer3')[0]
+		experiment_name = filename.split('/')[-1].split('.')[0]
+	experiment = Experiment.objects.get_or_create(name=experiment_name)[0]
 	print "Loading corpus"
+	n_done = 0
+	to_do = len(lda_dict['corpus'])
 	for doc in lda_dict['corpus']:
+		n_done += 1
+		if n_done % 100 == 0:
+			print "Done {}/{}".format(n_done,to_do)
 		metdat = jsonpickle.encode(lda_dict['doc_metadata'][doc])
 		d = Document.objects.get_or_create(name=doc,experiment=experiment,metadata=metdat)[0]
 		for word in lda_dict['corpus'][doc]:
 			feature = Feature.objects.get_or_create(name=word,experiment=experiment)[0]
 			fi = FeatureInstance.objects.get_or_create(document = d,feature = feature, intensity = lda_dict['corpus'][doc][word])
 	print "Loading topics"
+	n_done = 0
+	to_do = len(lda_dict['beta'])
 	for topic in lda_dict['beta']:
-		m2m = Mass2Motif.objects.get_or_create(name = topic,experiment = experiment)[0]
+		n_done += 1
+		if n_done % 100 == 0:
+			print "Done {}/{}".format(n_done,to_do)
+		metadata = {}
+		metadata = lda_dict['topic_metadata'].get(topic,{})
+		m2m = Mass2Motif.objects.get_or_create(name = topic,experiment = experiment,metadata = jsonpickle.encode(metadata))[0]
 		for word in lda_dict['beta'][topic]:
 			feature = Feature.objects.get(name = word,experiment=experiment)
 			Mass2MotifInstance.objects.get_or_create(feature = feature,mass2motif = m2m,probability = lda_dict['beta'][topic][word])
 	print "Loading theta"
+	n_done = 0
+	to_do = len(lda_dict['theta'])
 	for doc in lda_dict['theta']:
+		n_done += 1
+		if n_done % 100 == 0:
+			print "Done {}/{}".format(n_done,to_do)		
 		document = Document.objects.get(name = doc,experiment=experiment)
-		print document
 		for topic in lda_dict['theta'][doc]:
 			mass2motif = Mass2Motif.objects.get(name = topic,experiment = experiment)
 			DocumentMass2Motif.objects.get_or_create(document = document,mass2motif = mass2motif,probability = lda_dict['theta'][doc][topic])
 	print "Loading phi"
+	n_done = 0
+	to_do = len(lda_dict['phi'])
 	for doc in lda_dict['phi']:
+		n_done += 1
+		if n_done % 100 == 0:
+			print "Done {}/{}".format(n_done,to_do)
 		document = Document.objects.get(name = doc,experiment=experiment)
-		print document
 		for word in lda_dict['phi'][doc]:
 			feature = Feature.objects.get(name=word,experiment=experiment)
 			feature_instance = FeatureInstance.objects.get(document=document,feature=feature)
