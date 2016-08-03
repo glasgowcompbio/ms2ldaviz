9ea39a455a919ccdf6c57399914afd86ebf65fc6
= <=>
2016-08-03 15:12:03 +0100

    form glitch

--- ms2ldaviz/basicviz/forms.py
+++ ms2ldaviz/basicviz/forms.py
@@ -2,4 +2,4 @@ from django import forms
 from basicviz.models import Mass2Motif
 
 class Mass2MotifMetadataForm(forms.Form):
-	metadata = forms.CharField(max_length = 256,blank = True,widget=forms.TextInput(attrs={'size':'80'}))
+	metadata = forms.CharField(max_length = 256,required = False,widget=forms.TextInput(attrs={'size':'80'}))
