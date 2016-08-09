275fc948a26180b26781a15a4ca504a681699353
= <=>
2016-08-09 22:01:19 +0100

    more csv changes

--- ms2ldaviz/dump_validations.py
+++ ms2ldaviz/dump_validations.py
@@ -27,17 +27,22 @@ if __name__ == '__main__':
         writer.writerow(['msm_id','m2m_annotation','doc_id','doc_annotation','valid','probability','score'])
         for mass2motif in annotated_mass2motifs:
             print mass2motif
-            dm2ms = DocumentMass2Motif.objects.filter(mass2motif = mass2motif,probability__gte = p_thresh)
+            m2minstances = Mass2MotifInstance.objects.filter(mass2motif = mass2motif)
+            m2mfeatures = {}
+            for instance in m2minstances:
+                m2mfeatures[instance.feature] = instance.probability
+            dm2ms = DocumentMass2Motif.objects.filter(mass2motif = mass2motif,probability__gte = 0.02)
+            score = 0.0
             for dm2m in dm2ms:
-                score = 0.0
                 document = dm2m.document
                 feature_instances = FeatureInstance.objects.filter(document = document)
                 for instance in feature_instances:
-                    fm2m = FeatureMass2MotifInstance.objects.filter(featureinstance = instance,mass2motif = mass2motif)
-                    if fm2m:
-                        score += instance.intensity * fm2m[0].probability
+                    if instance.feature in m2mfeatures:
+                        fm2m = FeatureMass2MotifInstance.objects.filter(featureinstance = instance,mass2motif = mass2motif)
+                        if fm2m:
+                            score += m2mfeatures[instance.feature] * fm2m[0].probability
                 # outstring +='{},{},{},"{}",{}\n'.format(mass2motif.id,mass2motif.annotation,dm2m.document.id,dm2m.document.annotation.encode('utf8'),dm2m.validated)
                 doc_name = '"' + dm2m.document.display_name + '"'
                 annotation = '"' + mass2motif.annotation + '"'
-                writer.writerow([mass2motif.id,mass2motif.annotation.encode('utf8'),dm2m.document.id,doc_name.encode('utf8'),dm2m.validated,dm2m.probability,score])
+                writer.writerow([mass2motif.id,mass2motif.name,mass2motif.annotation.encode('utf8'),dm2m.document.id,doc_name.encode('utf8'),dm2m.validated,dm2m.probability,score])
 
