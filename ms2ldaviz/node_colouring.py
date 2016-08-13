cde6632205fea00d0e215b4c031e3b21220e3f5f
Laura Zebrauskaite <lauzebrau@gmail.com>
2016-08-13 19:52:31 +0100

    started

---
+++ ms2ldaviz/node_colouring.py
@@ -0,0 +1,14 @@
+import os
+import pickle
+import numpy as np
+import sys
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ms2ldaviz.settings")
+
+import django
+django.setup()
+
+import jsonpickle
+
+from basicviz.models import Experiment,Document,Feature,FeatureInstance,Mass2Motif,Mass2MotifInstance,DocumentMass2Motif,FeatureMass2MotifInstance
+
+print 'labas'
