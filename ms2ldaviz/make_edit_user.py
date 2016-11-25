e8785474c1153c00f9d9eec57a49f7544fc4eed3
= <=>
2016-11-25 19:45:09 +0000

    added script to give a particular user edit access to all experiments

--- ms2ldaviz/make_edit_user.py
+++ ms2ldaviz/make_edit_user.py
@@ -10,16 +10,9 @@ from basicviz.models import Experiment,User,UserExperiment
 
 if __name__ == '__main__':
 	username = sys.argv[1]
-	if len(sys.argv) > 2:
-		email = sys.argv[2]
-		pw = sys.argv[3]
-	user = User.objects.filter(username = username)
-	if len(user) == 0:
-		user = User.objects.create_user(username, email, pw)
-	else:
-		user = user[0]
 	experiments = Experiment.objects.all()
-
 	for experiment in experiments:
-		UserExperiment.objects.create(user = user,experiment = experiment,permission = 'view')
-
+		ue = UserExperiment.objects.get_or_create(user = user,experiment = experiment)[0]
+		ue.permission = 'edit'
+		ue.save()
+		
