23c1a1dd9f3db2ea3d1cadc1042b3d9881151f85
= <=>
2016-11-25 16:00:27 +0000

    improved user experiment admin view

--- ms2ldaviz/basicviz/admin.py
+++ ms2ldaviz/basicviz/admin.py
@@ -3,13 +3,17 @@ from django.contrib import admin
 from basicviz.models import Experiment, Document, Mass2Motif, Feature, FeatureMass2MotifInstance, VizOptions, \
     UserExperiment, ExtraUsers, MultiFileExperiment, MultiLink, Alpha, PeakSet, IntensityInstance, SystemOptions
 
+
+class UserExperimentAdmin(admin.ModelAdmin):
+	list_display = ('experiment','user','permission')
+
 admin.site.register(Experiment)
 admin.site.register(Document)
 admin.site.register(Mass2Motif)
 admin.site.register(Feature)
 admin.site.register(FeatureMass2MotifInstance)
 admin.site.register(VizOptions)
-admin.site.register(UserExperiment)
+admin.site.register(UserExperiment,UserExperimentAdmin)
 admin.site.register(ExtraUsers)
 admin.site.register(MultiFileExperiment)
 admin.site.register(MultiLink)
