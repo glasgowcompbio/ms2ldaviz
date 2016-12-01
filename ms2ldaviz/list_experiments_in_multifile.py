a0bcf2151ac4c88136af5e409f97011d2854bb71
= <=>
2016-12-01 15:33:05 +0000

    added listing method

---
+++ ms2ldaviz/list_experiments_in_multifile.py
@@ -0,0 +1,26 @@
+import os
+import pickle
+import numpy as np
+import sys
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ms2ldaviz.settings")
+
+import django
+django.setup()
+
+
+# This is to process files that have document names stored as mz_rt
+# it extracts the acutal mz and rt from the name and adds them to the metadata
+# This version works for multi-file experiments
+
+
+import jsonpickle
+import csv
+
+from basicviz.models import MultiFileExperiment,Experiment,Document,PeakSet,IntensityInstance
+
+if __name__ == '__main__':
+	multifile_experiment_name = sys.argv[1]
+	mfe = MultiFileExperiment.objects.get(name = multifile_experiment_name)
+	links = mfe.multilink_set.all()
+	for link in links:
+		print link.experiment
