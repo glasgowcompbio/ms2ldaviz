fecc6d13873f4d1132b1d6689f355c0683ebdcb9
= <=>
2016-12-14 12:33:30 +0000

    tiny migration

--- ms2ldaviz/annotation/models.py
+++ ms2ldaviz/annotation/models.py
@@ -24,10 +24,10 @@ class TaxaTerm(models.Model):
 class SubstituentInstance(models.Model):
 	subterm = models.ForeignKey(SubstituentTerm,null = False)
 	motif = models.ForeignKey(Mass2Motif,null = False)
-	probability = models.FloatField(null = False)
+	probability = models.FloatField(null = True)
 
 class TaxaInstance(models.Model):
 	taxterm = models.ForeignKey(TaxaTerm,null = False)
 	motif = models.ForeignKey(Mass2Motif,null = False)
-	probability = models.FloatField(null = False)
+	probability = models.FloatField(null = True)
 
