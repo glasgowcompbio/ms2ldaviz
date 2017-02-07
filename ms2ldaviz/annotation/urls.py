299c6f8b2d5fc312c00b3de0e53ea7bcb4f1911a
Joe Wandy <joe.wandy@glasgow.ac.uk>
2017-02-07 15:48:21 +0000

    Added a batch annotation API. Still can be made faster:
    “A lot of the time now is taking the beta values and turning them back into a matrix which can be done once.
    if we also have all of the features in a nice python list, then finding particular matches is really fast with 'bisect’”

--- ms2ldaviz/annotation/urls.py
+++ ms2ldaviz/annotation/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^start_annotation/(?P<basicviz_experiment_id>\w+)/$',views.start_annotation, name='start_annotation'),
     url(r'^query/(?P<basicviz_experiment_id>\w+)/$', views.query_annotation, name='query_annotation'),
+    url(r'^batch_query/(?P<db_name>\w+)/$', views.batch_query_annotation, name='batch_query_annotation'),
 ]
--- ms2ldaviz/annotation/views.py
+++ ms2ldaviz/annotation/views.py
@@ -1,5 +1,4 @@
 import json
-import jsonpickle
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
@@ -7,8 +6,10 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from basicviz.models import Experiment,UserExperiment
-from annotation.forms import AnnotationForm
-from lda_methods import annotate
+from .forms import AnnotationForm, BatchAnnotationForm
+from .lda_methods import annotate
+from .helpers import deprecated
+from .constants import ANNOTATE_DATABASES
 
 
 @login_required(login_url = '/basicviz/login/')
@@ -24,6 +25,7 @@ def index(request):
     
     context_dict = {'experiments':tokeep}
     return render(request,'annotation/index.html',context_dict)
+
 
 def parse_spectrum_string(spectrum_string):
     # Parse the spectrum that has been input
@@ -41,10 +43,10 @@ def parse_spectrum_string(spectrum_string):
             continue
         else:
             # If not MONA, assume that its just mz, rt pairs in a long list
-            if mz == None:
+            if mz is None:
                 # Must be a new peak
                 mz = float(token)
-            elif intensity == None:
+            elif intensity is None:
                 # Already have a mz so this must be an intensity
                 intensity = float(token)
                 # Store the peak and then forget the mz and intensity
@@ -54,14 +56,11 @@ def parse_spectrum_string(spectrum_string):
     return peaks
 
 
-
-
 @login_required(login_url = '/basicviz/login/')
 def start_annotation(request,basicviz_experiment_id):
     # Starts the annotation. User uploads a spectrum    
     experiment = Experiment.objects.get(id = basicviz_experiment_id)
     context_dict = {'experiment':experiment}
-
 
     if request.method == 'POST':
         form = AnnotationForm(request.POST)
@@ -91,9 +90,7 @@ def start_annotation(request,basicviz_experiment_id):
                     text += ": " + m.annotation
                 scatter_data.append((t,o,text))
 
-
             context_dict['scatter_data'] = json.dumps(scatter_data)
-
 
         else:
             context_dict['annotation_form'] = form
@@ -103,12 +100,14 @@ def start_annotation(request,basicviz_experiment_id):
 
     return render(request,'annotation/start_annotation.html',context_dict)
 
+
+# Replaced by batch_query_annotation()
+# I think Simon is still using this method in the notebooks..? Otherwise we can delete it.
+@deprecated
 @csrf_exempt
 def query_annotation(request, basicviz_experiment_id):
 
-    experiment = Experiment.objects.get(id=basicviz_experiment_id)
     response_data = {'status': 'OK'}
-
     if request.method == "POST":
 
         form = AnnotationForm(request.POST)
@@ -147,8 +146,62 @@ def query_annotation(request, basicviz_experiment_id):
             field_errors = [(field.label, field.errors) for field in form]
             response_data['status'] = 'ERROR: %s' % field_errors
 
-    else: # GET request
-        experiment = Experiment.objects.get(id=basicviz_experiment_id)
-        response_data['experiment_name'] = experiment.name
+    return JsonResponse(response_data)
+
+
+@csrf_exempt
+def batch_query_annotation(request, db_name):
+
+    # check valid db name
+    if db_name not in ANNOTATE_DATABASES.keys():
+        msg = 'ERROR: annotation can only be performed against %s' % ANNOTATE_DATABASES.keys()
+        response_data = {
+            'status': msg
+        }
+        return JsonResponse(response_data)
+
+    basicviz_experiment_id = ANNOTATE_DATABASES[db_name]
+    if request.method == "POST":
+
+        form = BatchAnnotationForm(request.POST)
+        if form.is_valid():
+
+            spectra_json = form.cleaned_data['spectra']
+            spectra = json.loads(spectra_json)
+            response_data = {'status': 'OK'}
+
+            for parentmass in spectra:
+
+                peaks = spectra[parentmass]
+                parentmass = float(parentmass)
+                spectrum = (parentmass, peaks)
+                document, motif_theta_overlap, plotdata, taxa_term_probs, sub_term_probs, matches_count = \
+                    annotate(spectrum, basicviz_experiment_id)
+
+                response_data[parentmass] = {}
+
+                taxa_terms = []
+                for taxa, prob in taxa_term_probs:
+                    taxa_terms.append((taxa.name, prob))
+                response_data[parentmass]['taxa_term_probs'] = taxa_terms
+
+                sub_terms = []
+                for sub, prob in sub_term_probs:
+                    sub_terms.append((sub.name, prob))
+                response_data[parentmass]['sub_term_probs'] = sub_terms
+
+                mto = []
+                for motif, theta, overlap in motif_theta_overlap:
+                    mto.append((motif.name, motif.annotation, theta, overlap))
+                response_data[parentmass]['motif_theta_overlap'] = mto
+
+                response_data[parentmass]['fragment_match'] = matches_count['fragment']
+                response_data[parentmass]['loss_match'] = matches_count['loss']
+                response_data[parentmass]['fragment_intensity_match'] = matches_count['fragment_intensity']
+                response_data[parentmass]['loss_intensity_match'] = matches_count['loss_intensity']
+
+        else: # form validation failed
+            field_errors = [(field.label, field.errors) for field in form]
+            response_data = {'status': 'ERROR: %s' % field_errors}
 
     return JsonResponse(response_data)
