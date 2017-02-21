dc801a99d6b8bb764d92097e6ce2fb70e1a8739b
Joe Wandy <joe.wandy@glasgow.ac.uk>
2017-02-21 12:35:35 +0000

    Added a basic pipeline structure. No celery yet.

--- ms2ldaviz/uploads/views.py
+++ ms2ldaviz/uploads/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from basicviz.models import Experiment, MultiFileExperiment, UserExperiment
 from django.shortcuts import render
 
-from basicviz.constants import EXPERIMENT_STATUS_CODE
+from basicviz.constants import EXPERIMENT_STATUS_CODE, EXPERIMENT_TYPE
 from basicviz.models import Experiment, UserExperiment
 from .forms import CreateExperimentForm
 
@@ -17,15 +17,20 @@ def create_experiment(request):
     if request.method == 'POST':
         experiment_form = CreateExperimentForm(request.POST, request.FILES)
         if experiment_form.is_valid():
+
             new_experiment = experiment_form.save(commit=False)
             pending_code, pending_msg = EXPERIMENT_STATUS_CODE[0] # pending
             new_experiment.status = pending_code
             new_experiment.csv_file = request.FILES.get('csv_file', None)
             new_experiment.mzml_file = request.FILES.get('mzml_file', None)
             new_experiment.save()
+
             user = request.user
             UserExperiment.objects.create(user=user, experiment=new_experiment, permission='edit')
+
+            process_experiment(new_experiment)
             return HttpResponseRedirect(reverse('index'))
+
         else:
             context_dict['experiment_form'] = experiment_form
     else:
@@ -33,3 +38,35 @@ def create_experiment(request):
         context_dict['experiment_form'] = experiment_form
 
     return render(request, 'uploads/add_experiment.html', context_dict)
+
+
+def process_experiment(exp):
+
+    pending, desc = EXPERIMENT_STATUS_CODE[0]
+    ready, desc = EXPERIMENT_STATUS_CODE[1]
+    if exp.status == pending:
+
+        # select the right pipeline
+        ms2lda, desc = EXPERIMENT_TYPE[0]
+        decomposition, desc = EXPERIMENT_TYPE[1]
+        pipelines = {
+            ms2lda: lda_pipeline,
+            decomposition: decomposition_pipeline
+        }
+        pipeline = pipelines[exp.experiment_type] # this is a function
+
+        # runs the correct pipeline based on the experiment type
+        pipeline(exp)
+
+        # finished
+        exp.status = ready
+
+
+def lda_pipeline(exp):
+    print 'Running LDA pipeline'
+    print exp.csv_file, exp.mzml_file
+
+
+def decomposition_pipeline(exp):
+    print 'Running decomposition pipeline'
+    print exp.csv_file, exp.mzml_file
