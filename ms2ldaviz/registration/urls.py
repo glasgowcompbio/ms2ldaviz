8ce0e31e512fd555e5b5868732e0658f9fd51ff7
Joe Wandy <joe.wandy@glasgow.ac.uk>
2017-03-30 15:40:33 +0100

    Added link to the default django admin change password screen. Doesn’t look good.

--- ms2ldaviz/registration/urls.py
+++ ms2ldaviz/registration/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from registration import views
+import django.contrib.auth.views as auth_views
 
 urlpatterns = [
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', views.user_login, name='login'),
     url(r'^logout/$', views.user_logout, name='logout'),
-    url(r'^profile/', views.ProfileUpdate.as_view(), name='profile'),
+    url(r'^profile/$', views.ProfileUpdate.as_view(), name='profile'),
+    url(r'^change_password/$', auth_views.password_change, {'post_change_redirect': 'profile'}, name='password_change'),
 ]
