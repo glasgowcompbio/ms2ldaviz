6a0778c3a3caf2ecbd8e837054f473db9688cd91
Ubuntu <ubuntu@ip-172-31-6-82.eu-west-1.compute.internal>
2017-04-06 23:02:12 +0000

    move process_ms1_analysis to celery

--- ms2ldaviz/ms1analysis/models.py
+++ ms2ldaviz/ms1analysis/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from basicviz.models import Document, Experiment
+from basicviz.constants import EXPERIMENT_STATUS_CODE
 
 class Sample(models.Model):
     name = models.CharField(max_length=32)
@@ -16,8 +17,11 @@ class Analysis(models.Model):
     name = models.CharField(max_length=128)
     description = models.CharField(max_length=1024)
     experiment = models.ForeignKey(Experiment)
-    group1 = models.CharField(max_length=256)
-    group2 = models.CharField(max_length=256)
+    group1 = models.CharField(max_length=2048)
+    group2 = models.CharField(max_length=2048)
+    ready_code, _ = EXPERIMENT_STATUS_CODE[1]
+    status = models.CharField(max_length=128, choices=EXPERIMENT_STATUS_CODE,
+                              null=True, default=ready_code)
 
 class AnalysisResult(models.Model):
     analysis = models.ForeignKey(Analysis)
--- ms2ldaviz/ms1analysis/views.py
+++ ms2ldaviz/ms1analysis/views.py
@@ -5,9 +5,11 @@ from django.shortcuts import render
 
 from basicviz.models import UserExperiment,JobLog, Document, Experiment
 from .forms import AnalysisForm
-from .models import Sample, DocSampleIntensity, AnalysisResult
+from .models import Sample, DocSampleIntensity, Analysis, AnalysisResult
 import numpy as np
 from scipy.stats import ttest_ind
+from ms2ldaviz.celery_tasks import app
+from basicviz.constants import EXPERIMENT_STATUS_CODE
 
 
 # Create your views here.
@@ -23,11 +25,18 @@ def create_ms1analysis(request, experiment_id):
         analysis_form = AnalysisForm(sample_choices, request.POST)
         if analysis_form.is_valid():
             new_analysis = analysis_form.save(commit=False)
+            pending_code, pending_msg = EXPERIMENT_STATUS_CODE[0]
+            new_analysis.status = pending_code
             new_analysis.experiment = experiment
             new_analysis.save()
 
             context_dict['analysis'] = new_analysis
-            process_ms1_analysis(new_analysis, analysis_form.cleaned_data, experiment_id)
+            params = {
+                'group1': analysis_form.cleaned_data['group1'],
+                'group2': analysis_form.cleaned_data['group2'],
+                'experiment_id': experiment_id
+            }
+            process_ms1_analysis.delay(new_analysis.id, params)
             return HttpResponseRedirect(reverse('index'))
 
         else:
@@ -39,9 +48,12 @@ def create_ms1analysis(request, experiment_id):
 
     return render(request, 'ms1analysis/add_ms1_analysis.html', context_dict)
 
-def process_ms1_analysis(new_analysis, cleaned_data, experiment_id):
-    group1 = cleaned_data['group1']
-    group2 = cleaned_data['group2']
+@app.task
+def process_ms1_analysis(new_analysis_id, params):
+    new_analysis = Analysis.objects.get(id=new_analysis_id)
+    group1 = params['group1']
+    group2 = params['group2']
+    experiment_id = params['experiment_id']
     group1_samples = [Sample.objects.filter(name=sample_name, experiment_id=experiment_id)[0] for sample_name in group1]
     group2_samples = [Sample.objects.filter(name=sample_name, experiment_id=experiment_id)[0] for sample_name in group2]
 
@@ -56,8 +68,8 @@ def process_ms1_analysis(new_analysis, cleaned_data, experiment_id):
             group2_intensities.append(DocSampleIntensity.objects.filter(sample=sample, document=document)[0].intensity)
         fold = np.mean(group1_intensities) / np.mean(group2_intensities)
         pValue = ttest_ind(group1_intensities, group2_intensities, equal_var = False)[1]
-        add_analysis_result(new_analysis, document, fold, pValue)
-        # print fold, pValue
-
-def add_analysis_result(new_analysis, document, fold, pValue):
-    result = AnalysisResult.objects.get_or_create(analysis=new_analysis, document=document, foldChange=fold, pValue=pValue)
+        # add_analysis_result(new_analysis, document, fold, pValue)
+        AnalysisResult.objects.get_or_create(analysis=new_analysis, document=document, foldChange=fold, pValue=pValue)
+        ready, _ = EXPERIMENT_STATUS_CODE[1]
+        new_analysis.status = ready
+        new_analysis.save()
