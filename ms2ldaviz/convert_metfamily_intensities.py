2ef7bf306ff4f4b4adfd915963c8ebdeb325df4e
= <=>
2017-04-11 16:10:16 +0100

    added a one-off script to convert metfamily

---
+++ ms2ldaviz/convert_metfamily_intensities.py
@@ -0,0 +1,27 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ms2ldaviz.settings")
+
+import django
+django.setup()
+
+import jsonpickle
+
+from basicviz.models import Experiment
+from ms1analysis.models import Sample,DocSampleIntensity
+
+
+if __name__ == '__main__':
+	metfamily = Experiment.objects.get(name = 'metfamily')
+	docs = metfamily.document_set.all()
+	print "Found {} documents".format(len(docs))
+	doc0 = docs[0]
+	md = jsonpickle.decode(doc0.metadata)
+	sample_dict = {}
+	for key in md['intensities']:
+		sample_dict[key],status = Sample.objects.get_or_create(name = key,experiment = metfamily)
+
+	for document in docs:
+		for samp,intensity in jsonpickle.decode(document.metadata)['intensities'].items():
+			ds,status = DocSampleIntensity.objects.get_or_create(document = document,sample = sample_dict[samp])
+			ds.intensity = intensity
+			ds.save() 
