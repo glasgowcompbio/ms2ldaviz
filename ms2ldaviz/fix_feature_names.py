5776ce7c5205ad862c5c57a634b8f5d687879fb4
= <=>
2017-12-15 08:46:59 +0000

    nothing

--- ms2ldaviz/fix_feature_names.py
+++ ms2ldaviz/fix_feature_names.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
     bfs = BVFeatureSet.objects.get(name = 'binned_005')
     features = Feature.objects.filter(featureset = bfs)
     frags = [f for f in features if f.name.startswith('fragment') and len(f.name.split('.')[-1])>4]
+    print frags[1].name
     # loss = [f for f in features if f.name.startswith('loss')]
     print len(frags)
     
