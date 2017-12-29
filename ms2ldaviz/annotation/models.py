8157a4492e4a07c4dc4fa1095a166dc0b8aad264
= <=>
2017-12-29 13:42:41 +0000

    added extra to annotation models

--- ms2ldaviz/annotation/models.py
+++ ms2ldaviz/annotation/models.py
@@ -25,9 +25,12 @@ class SubstituentInstance(models.Model):
 	subterm = models.ForeignKey(SubstituentTerm,null = False)
 	motif = models.ForeignKey(Mass2Motif,null = False)
 	probability = models.FloatField(null = True)
+	z_score = models.FloatField(null = True)
+
 
 class TaxaInstance(models.Model):
 	taxterm = models.ForeignKey(TaxaTerm,null = False)
 	motif = models.ForeignKey(Mass2Motif,null = False)
 	probability = models.FloatField(null = True)
+	z_score = models.FloatField(null = True)
 
