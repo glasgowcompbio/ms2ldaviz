15262117d8326472c3d16265dc69055fd13bc873
Simon Rogers <simon.d.rogers@gmail.com>
2018-01-05 08:25:03 +0000

    change to massDiff notebook

--- code/ms2lda_feature_extraction_utilities.py
+++ code/ms2lda_feature_extraction_utilities.py
@@ -10,3 +10,33 @@ def reverse_corpus(corpus):
 				reverse_corpus[f] = set()
 			reverse_corpus[f].add(doc)
 	return reverse_corpus
+
+# Counts how many docs each feature appears in
+def count_docs(reverse_corpus,corpus,min_percent = 0.0,max_percent = 100.0):
+	doc_counts = {}
+	n_docs = len(corpus)
+	for feature,docs in reverse_corpus.items():
+		doc_counts[feature] = (len(docs),(100.0*len(docs))/n_docs)
+	df = zip(doc_counts.keys(),doc_counts.values())
+	to_remove = []
+	if min_percent > 0.0:
+		df2 = filter(lambda x: x[1][1] < min_percent,df)
+		tr,_ = zip(*df2)
+		to_remove += tr
+	if max_percent < 100.0:
+		df2 = filter(lambda x: x[1][1] > max_percent,df)
+		tr,_ = zip(*df2)
+		to_remove += tr
+	to_remove = set(to_remove)
+
+	return doc_counts,to_remove
+
+# remove a particular set of features from the corpus
+def remove_features(corpus,to_remove):
+	for doc,spectrum in corpus.items():
+		features = set(spectrum.keys())
+		overlap = features.intersection(to_remove)
+		for f in overlap:
+			del spectrum[f]
+	return corpus
+
