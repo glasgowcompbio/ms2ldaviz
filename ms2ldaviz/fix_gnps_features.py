dac844bab5901c278977370d993ba60c9705319e
= <=>
2018-02-07 09:58:46 +0000

    script to fix the gnps features

--- ms2ldaviz/fix_gnps_features.py
+++ ms2ldaviz/fix_gnps_features.py
@@ -7,6 +7,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ms2ldaviz.settings")
 import django
 django.setup()
 
+from django.db import transaction
+
 from basicviz.models import *
 
 # Script to migrate the gnps_binned_005 experiment over to the binned_005 featureset
@@ -26,18 +28,26 @@ if __name__ == '__main__':
 
 	gnps_fi = FeatureInstance.objects.filter(document__in = gnps_docs)
 
-	for fi in gnps_fi:
-		gnps_f = fi.feature
-		# reformat name just in case
-		tokens = gnps_f.name.split('_')
-		newname = tokens[0] + "_{:.4f}".format(float(tokens[1]))
-		print newname
-		if newname in feat_dict:
-			# fi.feature = feat_dict[newname]
-			# fi.save()
-			print "Found"
-		else:
-			print "Not found"
-			# gnps_f.name = newname
-			# gnps_f.featureset = fs
-			# gnps_f.save()
+	total = len(gnps_fi)
+	n_done = 0
+
+	with transaction.atomic():
+		for fi in gnps_fi:
+			gnps_f = fi.feature
+			# reformat name just in case
+			tokens = gnps_f.name.split('_')
+			newname = tokens[0] + "_{:.4f}".format(float(tokens[1]))
+			if newname in feat_dict:
+				global_feature = feat_dict[newname]
+				fi.feature = global_feature
+				fi.save()
+			else:
+				gnps_f.name = newname # ensure correct format
+				gnps_f.featureset = fs
+				gnps_f.save()
+			n_done += 1
+			if n_done % 1000 == 0:
+				print n_done,total
+
+	e.featureset = fs
+	e.save()
