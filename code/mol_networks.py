4eddf82dada333e2ceec9ae8ca8b80970dc67b1e
= <=>
2018-02-13 20:31:47 +0000

    network class made

--- code/mol_networks.py
+++ code/mol_networks.py
@@ -3,58 +3,9 @@
 
 from pprint import PrettyPrinter
 import numpy as np
-def modified_cosine(spec1,spec2,tol = 0.3):
-	# compute the modified cosine
-	# spec1 and spec2 are lists of (mz,i) tuples
-	# spectra *must* be sorted in ascending mz
-	# and normalised such that biggest peak = 100
-	pp = PrettyPrinter()
-
-	pp.pprint(spec1)
-	pp.pprint(spec2)
-
-	norm1 = 0.0
-	norm2 = 0.0
-
-	spec2_start = 0 # start of range to search in
-	total = 0.0
-	# finished = False
-	# for peak in spec1:
-	# 	biggest = 0.0
-	# 	while peak[0]-spec2[spec2_start][0] > tol:
-	# 		spec2_start += 1
-	# 		if spec2_start == len(spec2):
-	# 			finished = True
-	# 			break
-	# 	if finished:
-	# 		break
-
-	# 	spec2_end = spec2_start
-
-	# 	while spec2[spec2_end][0] - peak[0] < tol:
-	# 		spec2_end += 1
-	# 		if spec2_end == len(spec2):
-	# 			break
-
-	# 	for peak2 in spec2[spec2_start:spec2_end]:
-	# 		print peak2
-	# 	print peak,spec2[spec2_start],spec2[spec2_end]
-	# 	break
-	total = 0.0
-	for peak1 in spec1:
-		biggest = 0.0
-		for peak2 in spec2:
-			if np.abs(peak1[0]-peak2[0]) <= tol:
-				biggest = max(biggest,np.sqrt(peak1[1])*np.sqrt(peak2[1]))
-				# biggest = peak1[1]*peak2[1]
-		total += biggest
-	norm1 = np.sqrt(sum([x[1] for x in spec1]))
-	norm2 = np.sqrt(sum([x[1] for x in spec2]))
-
-	return 1.0*(total)/(norm1*norm2)
 
 
-
+# utility function to create an edge dictionary
 def create_edge_dict(scores,ms1,min_frag_overlap = 6,min_score = 0.7):
 	edge_dict = {}
 	for score_row in scores:
@@ -67,90 +18,153 @@ def create_edge_dict(scores,ms1,min_frag_overlap = 6,min_score = 0.7):
 				edge_dict[mol1] = []
 			if not mol2 in edge_dict:
 				edge_dict[mol2] = []
-			edge_dict[mol1].append((mol2,score))
-			edge_dict[mol2].append((mol1,score))
+			edge_dict[mol1][mol2] = score
+			edge_dict[mol2][mol1] = score
 
-	for mol,edges in edge_dict.items():
-		sorted_edges = sorted(edges,key = lambda x: x[1],reverse = True)
-		edge_dict[mol] = sorted_edges
 	return edge_dict
 
+class Network(object):
+	def __init__(self,edge_dict):
+		assert self._is_symmetric(edge_dict), "Edge dictionary not symmetric"
+		self.edge_dict = edge_dict
+		self._compute_degree()
 
-def top_k_filter(edge_dict,k = 10):
-	for mol,edges in edge_dict.items():
-		if len(edges) > k:
-			edges = edges[:k]
-			edge_dict[mol] = edges
-	# ensure symmetry
-	n_edges = {}
-	filtered_edge_dict = {}
-	for mol,edges in edge_dict.items():
-		new_edges = []
-		remove_index = []
-		for i,edge in enumerate(edges):
-			mol2,score = edge
-			targetmols,_ = zip(*edge_dict[mol2])
-			if mol in targetmols:
-				new_edges.append(edge)
-		filtered_edge_dict[mol] = new_edges
-		
-		n_edges[mol] = len(new_edges)
-	return filtered_edge_dict,n_edges
 
-def make_network(edge_dict,max_nodes = 100):
-	components = {}
-	# remove the singletons
-	sing_id = -1
-	done = set()
-	component_index = 0
-	for mol,edges in edge_dict.items():
-		if len(edges) == 0:
-			components[mol] = sing_id
-			done.add(mol)
-	component_thresholds = {}
-	for mol in edge_dict:
-		if mol in done:
-			continue
-		# build a component from this mol
-		too_big = True
-		min_score = 0.7
-		while too_big:
-			min_seen = 1.0
-			finished = False
-			search_pos = 0
-			members = [mol]
-			done.add(mol)
-			while not finished:
-				c = members[search_pos]
-				target_scores = edge_dict[c]
-				for t,s in target_scores:
-					if (not t in done) and (s > min_score):
-						members.append(t)
-						done.add(t)
-						if s < min_seen:
-							min_seen = s
-				search_pos += 1
-				if search_pos == len(members):
-					finished = True
-			n_members = len(members)
-			# print n_members
-			if n_members <= max_nodes:
-				too_big = False
+	def _is_symmetric(self,edge_dict):
+		# check each edge is in there twice
+		for mol,edges in edge_dict.items():
+			for target,score in edges.items():
+				try:
+					s2 = edge_dict[target][mol]
+					if not s2 == score:
+						return False
+				except:
+					return False
+		return True
+
+
+
+	def _compute_degree(self):
+		self.n_edges = {}
+		for mol in self.edge_dict:
+			self.n_edges[mol] = len(self.edge_dict[mol])
+	
+	def top_k_filter(k=10):
+		# create a sorted version of the dictionary
+		min_scores = {}
+		for mol,edges in edge_dict.items():
+			edge_score = zip(edges.keys(),edges.values())
+			edge_score = sorted(edge_score,key = lambda x: x[1],reverse = True)
+			if len(edge_score) < k:
+				min_scores[mol] = 0.0
 			else:
-				min_score = min_seen
-				done = done - set(members)
-			# print min_seen
-		print len(members)
-		if n_members == 1:
-			components[mol] = sing_id
-		else:
-			for mol2 in members:
-				components[mol2] = component_index
-			component_thresholds[component_index] = min_score
-			component_index += 1
-
-		
+				min_scores[mol] = edge_score[k-1][1]
 
 
-	return components,component_thresholds
+		filtered_edge_dict = {}
+		n_edges = {}
+		for mol,edges in edge_dict.items():
+			filtered_edge_dict[mol] = {}
+			for target_mol,score in edges.items():
+				if score >= min_scores[mol] and edge_dict[target_mol][mol] >= min_scores[target_mol]:
+					# keep the edge
+					filtered_edge_dict[mol][target_mol] = score
+			n_edges[mol] = len(filtered_edge_dict[mol])
+		self.edge_dict = filtered_edge_dict
+		self.n_edges = n_edges
+
+
+	def _extract_component(self,min_score = 0.5,start_node = None):
+		if not start_node:
+			start_node = self.edge_dict.keys()[0]
+		component_nodes = [start_node]
+		search_pos = 0
+		finished = False
+		while not finished:
+			edges = self.edge_dict[component_nodes[search_pos]]
+			for target_node,score in edges.items():
+				if score >= min_score and not target_node in component_nodes:
+					component_nodes.append(target_node)
+			search_pos += 1
+			if search_pos == len(component_nodes):
+				finished = True
+
+		# create the new edge dict
+		new_edge_dict = {}
+		for mol in component_nodes:
+			new_edge_dict[mol] = {}
+			for target,score in self.edge_dict[mol].items():
+				if target in component_nodes:
+					new_edge_dict[mol][target] = score
+
+		self._remove_nodes(component_nodes)
+		return Network(new_edge_dict)
+
+	def _remove_nodes(self,nodes):
+		for node in nodes:
+			edges = self.edge_dict[node]
+			for target,score in edges.items():
+				del self.edge_dict[target][node]
+			del self.edge_dict[node]
+
+	def _find_lowest_thresh_edge(self):
+		mol1 = self.edge_dict.keys()[0]
+		mol2 = self.edge_dict[mol1].keys()[0]
+		lowest_thresh = self.edge_dict[mol1][mol2]
+		lowest_pair = (mol1,mol2)
+		for mol,edges in self.edge_dict.items():
+			for target,score in edges.items():
+				if score <= lowest_thresh:
+					lowest_thresh = score
+					lowest_pair = (mol,target)
+		return lowest_thresh,lowest_pair
+
+	def _cut(self,min_score = 0.5):
+		finished = False
+		sub_network = None
+		while not finished:
+			lowest_thresh,lowest_pair = self._find_lowest_thresh_edge()
+			# remove the edge
+			del self.edge_dict[lowest_pair[0]][lowest_pair[1]]
+			del self.edge_dict[lowest_pair[1]][lowest_pair[0]]
+			sub_network = self._extract_component(min_score = min_score,start_node = lowest_pair[0])
+			# check if that's the entire network and if it is, we need to remove another edge
+			if len(self.edge_dict) == 0:
+				self.edge_dict = sub_network.edge_dict
+				self._compute_degree()
+			else:
+				finished = True
+		return sub_network
+
+	def n_nodes(self):
+		return len(self.edge_dict)
+
+	def convert_to_components(self,min_score=0.5,max_component_size = 100):
+		components = []
+		while self.n_nodes() > 0:
+			components.append(self._extract_component(min_score = min_score))
+		small_enough = []
+		needs_cropping = []
+		for component in components:
+			if component.n_nodes() > max_component_size:
+				needs_cropping.append(component)
+			else:
+				small_enough.append(component)
+
+		while len(needs_cropping) > 0:
+			new_crop = []
+			for component in needs_cropping:
+				new_comp = component._cut(min_score = 0.5)
+				if new_comp.n_nodes() > max_component_size:
+					new_crop.append(new_comp)
+				else:
+					small_enough.append(new_comp)
+				if component.n_nodes() > max_component_size:
+					new_crop.append(component)
+				else:
+					small_enough.append(component)
+			needs_cropping = new_crop
+
+
+		return small_enough
 
