810efe0547eebbcf8a9230eab83fd52bea971a56
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2018-08-20 13:33:05 +0200

    Compute overlap scores directly after lda
    Using compute_overlap_scores() supplied by @sdrogers

--- ms2ldaviz/run_gensim.py
+++ ms2ldaviz/run_gensim.py
@@ -112,6 +112,22 @@ def msfile2corpus(ms2_file, ms2_format, min_ms1_intensity, min_ms2_intensity, mz
     json.dump(lda_dict, corpusjson)
 
 
+def compute_overlap_scores(lda_dictionary):
+    # Compute the overlap scores for the lda model in dictionary format
+    overlap_scores = {}
+    for doc,phi in lda_dictionary['phi'].items():
+        motifs = lda_dictionary['theta'][doc].keys()
+        doc_overlaps = {m : 0.0 for m in motifs}
+        for word, probs in phi.items():
+            for m in motifs:
+                if word in lda_dictionary['beta'][m] and m in probs:
+                    doc_overlaps[m] += lda_dictionary['beta'][m][word]*probs[m]
+        overlap_scores[doc] = {}
+        for m in doc_overlaps:
+            overlap_scores[doc][m] = doc_overlaps[m]
+    return overlap_scores
+
+
 def gensim(corpusjson, ldajson,
            n, k, gamma_threshold,
            chunksize, batch, normalize, passes,
@@ -166,6 +182,7 @@ def gensim(corpusjson, ldajson,
     lda_dict['theta'] = theta
     lda_dict['phi'] = phi
     lda_dict['K'] = k
+    lda_dict['overlap_scores'] = compute_overlap_scores(lda_dict)
     json.dump(lda_dict, ldajson)
 
 
