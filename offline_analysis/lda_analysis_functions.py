cd11793cd4fc4e2cda3795268fb16e22bf68809d
Simon Rogers <simon.d.rogers@gmail.com>
2018-09-16 13:14:52 +0100

    fixed motif matching

--- offline_analysis/lda_analysis_functions.py
+++ offline_analysis/lda_analysis_functions.py
@@ -68,9 +68,9 @@ def plot_mol(lda_dict,mol,color_motifs = False,xlim = None,**kwargs):
 	motif_probs.sort(key = lambda x: x[1],reverse = True)
 
 	n_motifs = min(len(motif_probs),4)
-	top_motifs = set([m for m,p in motif_probs[:n_motifs]])
-	print top_motifs
+	top_motifs = [m for m,p in motif_probs[:n_motifs]]
 	cols = ['r','g','b','k']
+	motif_cols = {m:cols[i] for i,m in enumerate(top_motifs)}
 	if not color_motifs:
 		for feature,intensity in spec.items():
 			if feature.startswith('fragment'):
@@ -85,13 +85,15 @@ def plot_mol(lda_dict,mol,color_motifs = False,xlim = None,**kwargs):
 				cum_intensity = 0
 				col_index = 0
 				for motif,prob in pphi.items():
-					plt.plot([mz,mz],[cum_intensity,cum_intensity + prob*total_intensity],cols[col_index])
+					plt.plot([mz,mz],[cum_intensity,cum_intensity + prob*total_intensity],color=motif_cols.get(motif,[0.6,0.6,0.6]))
 					cum_intensity += prob*total_intensity
 					col_index += 1
 					if col_index == len(cols):
 						break
 				if cum_intensity < total_intensity:
 					plt.plot([mz,mz],[cum_intensity,total_intensity],'k',color = [0.6,0.6,0.6])
+		tit_string = "    ".join(["{}: {}".format(m,c) for m,c in motif_cols.items()])
+		plt.title(tit_string)
 	if xlim:
 		plt.xlim(xlim)
 
@@ -104,14 +106,18 @@ def match_motifs(lda_dict,db_motifs,threshold = 0.7):
 			if score > threshold:
 				matches.append((motif,db_motif,score))
 	matches.sort(key = lambda x: x[2], reverse = True)
+	return matches
 
 def compute_similarity(spec1,spec2):
     # compute the cosine similarity of the two spectra
-    prod = 0
-    i1 = 0
+    prod = 0.0
+    i1 = 0.0
     for mz,intensity in spec1.items():
         i1 += intensity**2
         intensity2 = spec2.get(mz,0.0)
         prod += intensity * intensity2
     i2 = sum([i**2 for i in spec2.values()])
-    return prod/(np.sqrt(i1)*np.sqrt(i2))
+    if i2 == 0:
+    	return 0.0
+    sim = prod/(np.sqrt(i1)*np.sqrt(i2))
+    return sim
