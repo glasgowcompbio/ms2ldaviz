5d312f53647cc101dcf7d33b05dac226060a727b
Simon Rogers <simon.d.rogers@gmail.com>
2018-11-17 10:16:06 +0000

    added functionality for comment field, just in the mdb object

--- ms2ldaviz/motifdb/models.py
+++ ms2ldaviz/motifdb/models.py
@@ -14,23 +14,13 @@ class MDBMotifSet(models.Model):
     featureset = models.ForeignKey(BVFeatureSet,null = True)
 
 class MDBMotif(Mass2Motif):
-    # name = models.CharField(max_length = 124,null = False)
     motif_set = models.ForeignKey(MDBMotifSet)
-    # annotation = models.TextField(null = True)
-    # short_annotation = models.TextField(null = True)
-    # comment = models.TextField(null = True)
-    # todo: add metadata
     def save(self,*args,**kwargs):
         super(MDBMotif,self).save(*args,**kwargs)
-
 
     def get_comment(self):
         md = jsonpickle.decode(super(MDBMotif,self).metadata)
         return md.get('comment',None)
 
     comment = property(get_comment)
-# class MDBMotifInstance(models.Model):
-#     motif = models.ForeignKey(MDBMotif)
-#     feature = models.ForeignKey(Feature)
-#     probability = models.FloatField(null = True)
     
