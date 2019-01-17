58b563bba84ad90d25aca0f408661a8b2a65f5fc
Joe Wandy <joewandy@gmail.com>
2019-01-17 13:56:42 +0000

    Changed url name to prevent clash with the index page

--- ms2ldaviz/motifdb/urls.py
+++ ms2ldaviz/motifdb/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from motifdb import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='motifdb_index'),
     url(r'^motif_set/(?P<motif_set_id>\w+)/$', views.motif_set,name='motif_set'),
     url(r'^motif/(?P<motif_id>\w+)/$', views.motif,name='motif'),
     url(r'^start_motif_matching/(?P<experiment_id>\w+)/$', views.start_motif_matching,name='start_motif_matching'),
