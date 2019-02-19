45e79e765be6bc4d99719b359ea0430178764a56
Simon Rogers <simon.d.rogers@gmail.com>
2019-02-19 09:04:12 +0000

    changed simons settings file

--- ms2ldaviz/ms2ldaviz/settings_simon.py
+++ ms2ldaviz/ms2ldaviz/settings_simon.py
@@ -1,22 +1,13 @@
 from settings import *
 
-# DEBUG = True
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DEBUG = True
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'simon',
-        'USER': 'simon',
-        'PASSWORD': 'ms2lda',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '',
     }
