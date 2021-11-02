679001cf63c650355fcea4b654dd048b11aa291c
Joe Wandy <joewandy@gmail.com>
2021-11-02 11:55:48 +0000

    Fixes to debug script

--- ms2ldaviz/debug_motifdb.py
+++ ms2ldaviz/debug_motifdb.py
@@ -1,10 +1,10 @@
 import requests
 
-server_url = 'http://ms2lda.org/motifdb/'
+server_url = 'https://ms2lda.org/motifdb/'
 # server_url = 'http://localhost/motifdb/'
 
 """Grabbing the latest Motifs from MS2LDA"""
-motifset_dict = requests.get(server_url + 'list_motifsets/', verify=False).json()
+motifset_dict = requests.get(server_url + 'list_motifsets/').json()
 db_list = []
 db_list.append(2)
 db_list.append(4)
@@ -16,16 +16,19 @@ db_list.append(16)
 
 # Acquire motifset from MS2LDA.org
 client = requests.session()
-token_output = client.get(server_url + 'initialise_api/', verify=False).json()
-token = token_output['token']
 
-data = {'csrfmiddlewaretoken': token}
+# no longer needed
+# token_output = client.get(server_url + 'initialise_api/', verify=False).json()
+# token = token_output['token']
+# data = {'csrfmiddlewaretoken': token}
+
+data = {}
 data['motifset_id_list'] = db_list
 data['filter'] = 'True'
 
-response = client.post(server_url + 'get_motifset/', data=data, verify=False)
+response = client.post(server_url + 'get_motifset/', data=data)
 json_output = response.json()
-print(json_output)
+# print(json_output)
 
 assert response.status_code == 200
 assert len(json_output['motifs']) > 0
