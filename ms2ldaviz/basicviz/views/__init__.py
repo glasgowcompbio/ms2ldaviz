1d3b579ceafbd724e5799e179f2ee87e3bd3a674
Joe Wandy <jwandy@metabolon.com>
2025-06-18 18:11:42 +0100

    Disabling stuff we don't need

--- ms2ldaviz/basicviz/views/__init__.py
+++ ms2ldaviz/basicviz/views/__init__.py
@@ -1,4 +1,16 @@
-from .views_index import *
-from .views_lda_single import *
-from .views_lda_multi import *
-from .views_lda_admin import *
+"""
+basicviz.views package – trimmed for the lightweight site.
+
+We publish only the modules that the URL-conf still needs.
+Nothing here should import NumPy, SciPy, RDKit, scikit-learn, etc.
+"""
+
+from importlib import import_module
+
+for _name in ("views_index", "views_lda_admin"):
+    import_module(f"{__name__}.{_name}")
+
+# from .views_index import *
+# from .views_lda_single import *
+# from .views_lda_multi import *
+# from .views_lda_admin import *
